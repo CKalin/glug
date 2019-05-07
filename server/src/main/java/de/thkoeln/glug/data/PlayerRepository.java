fcd7403186d11faa2a3dcacd269cfe063ee6fda2
Steffen Grewing <grewing@admins-MacBook-Pro.local>
2019-05-07 16:06:57 +0200

    generic type fixed

--- server/src/main/java/de/thkoeln/glug/data/PlayerRepository.java
+++ server/src/main/java/de/thkoeln/glug/data/PlayerRepository.java
@@ -2,6 +2,6 @@ package de.thkoeln.glug.data;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlayerRepository extends CrudRepository<Challenge, Integer> {
+public interface PlayerRepository extends CrudRepository<Player, Integer> {
 
 }

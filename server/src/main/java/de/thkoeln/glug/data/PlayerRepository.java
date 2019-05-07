eabaf8abb3e50927d5ea353e593627608a3380f1
Steffen Grewing <grewing@admins-MacBook-Pro.local>
2019-05-07 16:33:30 +0200

    query method to find player by name

--- server/src/main/java/de/thkoeln/glug/data/PlayerRepository.java
+++ server/src/main/java/de/thkoeln/glug/data/PlayerRepository.java
@@ -3,5 +3,5 @@ package de.thkoeln.glug.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
-
+	Iterable<Player> findByName(String name);
 }

ed4b2e79087e2071c68447b9252c0e5bdc591e25
Steffen Grewing <grewing@MacBook-Steffen.fritz.box>
2019-05-21 00:11:56 +0200

    test challengeFactory added

---
+++ server/src/test/java/de/thkoeln/glug/data/ChallengeFactoryTest.java
@@ -0,0 +1,23 @@
+package de.thkoeln.glug.data;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ChallengeFactoryTest {
+
+	@Test
+    public void generateRandomChallenge() {
+		QuizChallenge challenge = ChallengeFactory.generateRandomChallenge();
+		assertNotNull(challenge.getColorBackground());
+		assertNotNull(challenge.getColorObject());
+		assertNotNull(challenge.getColorObjectBorder());
+		assertNotNull(challenge.getColorText());
+		assertNotNull(challenge.getQuestion());
+		assertNotNull(challenge.getQuestionType());
+		assertNotNull(challenge.getText());
+		assertNotNull(challenge.getAnswers());
+		assertNotNull(challenge.getShape());
+    }
+
+}
